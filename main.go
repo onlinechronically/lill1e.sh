5c5fd27798347dc16259e46d58222653b8216036
lillie <onlinechronically@protonmail.com>
2024-05-09 22:19:45 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	serverRouter := mux.NewRouter()
+	serverRouter.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))))
+	serverRouter.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "views/construction.html")
+	})
+	serverRouter.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "views/construction.html")
+	})
+	err := http.ListenAndServe(":8080", serverRouter)
+	if err != nil {
+		log.Fatal("There was an error starting the server: ", err)
+	}
+}
